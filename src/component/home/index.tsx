8e0c4193381499d611705d4004704b7446eccfe6
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-06-12 14:54:41 -0700

    Basic table viewing

--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -1,6 +1,6 @@
-import {Carousel} from "react-bootstrap";
-import {useEffect} from "react";
-import Papa from "papaparse";
+import {Button, Carousel, Modal} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import Papa, {ParseResult} from "papaparse";
 
 const Video = (props: { src: string }) => {
     return (
@@ -12,6 +12,27 @@ const Video = (props: { src: string }) => {
 }
 
 const VideoPair = () => {
+    const [showModal, setShowModal] = useState(true);
+    const [csvResult, setCsvResult] = useState<Array<Array<String>>>([[]]);
+
+    useEffect(() => {
+        fetch("data/data.csv")
+            .then(response => response.text())
+            .then((dataStr) => {
+                const parseResult: ParseResult<any> = Papa.parse(dataStr);
+                const csvResult: Array<Array<string>> = parseResult.data;
+                setCsvResult(csvResult);
+            })
+    }, []);
+
+    // const csvResultStr = csvResult.map((row) =>
+    //     <tr key={row.toString()}>
+    //         <td>
+    //             {row.toString()}
+    //         </td>
+    //     </tr>
+    // );
+
     return (
         <div style={{
             display: "flex",
@@ -22,20 +43,35 @@ const VideoPair = () => {
         }}>
             <Video src={"video/vid1.mp4"}/>
             <Video src={"video/vid2.mp4"}/>
+
+            <Modal show={showModal} onHide={() => {setShowModal(false)}}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <table>
+                        {
+                            csvResult.map((row) =>
+                                <tr key={row.toString()}>
+                                    <td>
+                                        {row.toString()}
+                                    </td>
+                                </tr>
+                            )
+                        }
+                    </table>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => {setShowModal(false)}}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
 
 const HomePage = () => {
-    useEffect(() => {
-        fetch("http://localhost:3000/data/data.csv")
-            .then(response => response.text())
-            .then((dataStr) => {
-                const parseResult = Papa.parse(dataStr);
-                console.log("result", parseResult.data);
-            })
-    });
-
     return (
         <div style={{width: "100%", height: "100%", background: "black"}}>
             <Carousel>
