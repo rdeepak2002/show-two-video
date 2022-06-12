9f8e0f0355655a8f5c1881d82d8d10a702b52c12
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-06-12 16:51:51 -0700

    Move video config to new file

--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -1,34 +1,9 @@
 import {Carousel} from "react-bootstrap";
 import {useState} from "react";
 import VideoPair from "./VideoPair";
+import {videoData} from "../../config/config";
 
 const HomePage = () => {
-    const videoData: any = [
-        {
-            title: "Trial 1",
-            dataSrc: "data/data.csv",
-            video1Src: "video/vid1.mp4",
-            video2Src: "video/vid2.mp4"
-        },
-        {
-            title: "Trial 2",
-            dataSrc: "data/data.csv",
-            video1Src: "video/vid3.mp4",
-            video2Src: "video/vid4.mp4"
-        },
-        {
-            title: "Trial 3",
-            dataSrc: "data/data.csv",
-            video1Src: "video/vid1.mp4",
-            video2Src: "video/vid2.mp4"
-        },
-        {
-            title: "Trial 4",
-            dataSrc: "data/data.csv",
-            video1Src: "video/vid1.mp4",
-            video2Src: "video/vid2.mp4"
-        },
-    ];
     const [index, setIndex] = useState(0);
     const handleSelect = (selectedIndex: any, e: any) => {
         setIndex(selectedIndex);
