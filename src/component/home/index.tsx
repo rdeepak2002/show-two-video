3ae03b30f5e012c37c4393e31150db8579453c23
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-06-12 15:11:37 -0700

    View data and title for each video pair

--- src/component/home/index.tsx
+++ src/component/home/index.tsx
@@ -12,7 +12,7 @@ const Video = (props: { src: string }) => {
 }
 
 const VideoPair = () => {
-    const [showModal, setShowModal] = useState(true);
+    const [showModal, setShowModal] = useState(false);
     const [csvResult, setCsvResult] = useState<Array<Array<String>>>([[]]);
 
     useEffect(() => {
@@ -36,13 +36,20 @@ const VideoPair = () => {
     return (
         <div style={{
             display: "flex",
+            flexDirection: "column",
             alignContent: "center",
             justifyContent: "center",
             alignItems: "center",
             height: "100%"
         }}>
-            <Video src={"video/vid1.mp4"}/>
-            <Video src={"video/vid2.mp4"}/>
+            <h3 style={{color: "white"}}>Title</h3>
+            <div style={{display: "flex", flexDirection: "row", flex: 0.9}}>
+                <Video src={"video/vid1.mp4"}/>
+                <Video src={"video/vid2.mp4"}/>
+            </div>
+            <Button variant="light" onClick={() => {
+                setShowModal(true)}
+            }>View Data</Button>
 
             <Modal show={showModal} onHide={() => {setShowModal(false)}}>
                 <Modal.Header closeButton>
@@ -66,9 +73,14 @@ const VideoPair = () => {
 }
 
 const HomePage = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex: any, e: any) => {
+        setIndex(selectedIndex);
+    };
+
     return (
         <div style={{width: "100%", height: "100%", background: "black"}}>
-            <Carousel>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
                     <VideoPair/>
                 </Carousel.Item>
