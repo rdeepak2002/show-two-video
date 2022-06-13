2de46137182e734d3a48393c075da636a79745b3
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-06-12 17:30:58 -0700

    Add deploy for production instructions

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 
+// generate config json
+const generateConfigJson = () => {
+    const configJsonPath = path.join(__dirname, "public/config/config.json");
+    console.log("OUTPUT CONFIG TO: " + configJsonPath);
+}
+
+generateConfigJson();
+
 // add middlewares
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static("public"));
